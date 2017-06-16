2e40396aa6030fbbe61f5731892306a05474339b
Kang-Jack <laserk_klei@hotmail.com>
2017-06-16 14:31:12 +0800

    dict gen

---
+++ vpCH1/pwDictGen.py
@@ -0,0 +1,13 @@
+import itertools
+#characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+characters = "abcdefghijklmnopqrstuvwxyz"
+dic=open("pass1.txt","a")
+##iterate all possible lengths of the password
+#for leng in range(1, len(characters)+1):
+    #print "lenght of password: ", leng
+    ##create an iterator over the cartesian product of all possible permuations
+    #it = itertools.product(characters, repeat=leng) #pass word length
+it = itertools.product(characters, repeat=6)
+for i in it:
+    dic.write("".join(i))
+dic.close()
